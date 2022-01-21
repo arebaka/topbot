4e18c05aa3eb6ca62076a7b2fa7ef18ebcaf29a2
arelive <me@are.moe>
2022-01-21 21:31:47 +0300

    improve perfomance

--- image.js
+++ image.js
@@ -7,21 +7,42 @@ const { durationFormatter, sizeFormatter } = require("human-readable");
 
 const config = require("./config");
 
-const browser = puppeteer.launch();
+let browser;
+let page;
+
+async function prepare()
+{
+	browser = await puppeteer.launch();
+	page    = await browser.newPage();
+
+	await page.exposeFunction("formatDur", durationFormatter());
+	await page.exposeFunction("formatSize", sizeFormatter({ std: "IEC" }));
+}
+
+const promise = prepare();
 
 module.exports = async () => {
-	const page = await (await browser).newPage();
+	await promise;
 	await page.goto(`file://${path.resolve("./views/" + config.image.sample)}`);
 
-	let data     = await si.getAllData();
+	let data = {};
+
 	data.user    = os.userInfo();
 	data.loadavg = os.loadavg();
 	data.env     = process.env;
 
-	await page.exposeFunction("formatDur", durationFormatter());
-	await page.exposeFunction("formatSize", sizeFormatter({ std: "IEC" }));
+	data.graphics    = await si.graphics();
+	data.os          = await si.osInfo();
+	data.system      = await si.system();
+	data.cpu         = await si.cpu();
+	data.time        = await si.time();
+	data.mem         = await si.mem();
+	data.currentLoad = await si.currentLoad();
+	data.temp        = await si.cpuTemperature();
+	data.fsSize      = await si.fsSize();
 
 	await page.evaluate(data => render(data), data);
+
 	const container = await page.$(config.image.select);
 
 	return await container.screenshot({ type: "jpeg", quality: 100 });
